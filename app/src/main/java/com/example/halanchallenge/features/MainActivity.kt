dda730b47086b47c81e455d8fc0a65e9fdaec10f
Yosry <yousry_badr@outlook.com>
2022-02-01 22:35:06 +0200

    create MainActivity in Features Package

---
+++ app/src/main/java/com/example/halanchallenge/features/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.halanchallenge.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.halanchallenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
