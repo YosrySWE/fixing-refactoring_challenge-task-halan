1d108beef60f19ab6fb525692d1f6ceca7b442e1
Yosry <yousry_badr@outlook.com>
2022-02-01 23:18:26 +0200

    comment AndroidNetworking statements

--- app/src/main/java/com/example/halanchallenge/features/list/ProductsListActivity.kt
+++ app/src/main/java/com/example/halanchallenge/features/list/ProductsListActivity.kt
@@ -1,23 +1,15 @@
 package com.example.halanchallenge.features.list
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.androidnetworking.AndroidNetworking
-import com.androidnetworking.common.Priority
-import com.androidnetworking.error.ANError
-import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.bumptech.glide.Glide
-import com.example.halanchallenge.features.list.ProductsAdapter.ItemClickListener
 import com.example.halanchallenge.R
 import com.example.halanchallenge.features.login.LoginResponse
 import com.google.gson.Gson
-import org.json.JSONObject
 
 class ProductsListActivity : AppCompatActivity() {
     private var response: String = ""
@@ -38,7 +30,7 @@ class ProductsListActivity : AppCompatActivity() {
             response = bundle.getString("RESPONSE").toString()
         }
         val gson = Gson()
-        loginResponse = gson.fromJson(response, LoginResponse::class.java)?: LoginResponse()
+        loginResponse = gson.fromJson(response, LoginResponse::class.java) ?: LoginResponse()
         userName = findViewById(R.id.username_tv)
         phoneNumber = findViewById(R.id.phone_number_tv)
         email = findViewById(R.id.email_tv)
@@ -55,37 +47,37 @@ class ProductsListActivity : AppCompatActivity() {
         logoutIV.setOnClickListener { finish() }
         val mLayoutManager = LinearLayoutManager(applicationContext)
         productsListRV.layoutManager = mLayoutManager
-        AndroidNetworking.initialize(applicationContext)
-        AndroidNetworking.get("https://assessment-sn12.halan.io/products")
-            .addHeaders("Authorization", "Bearer " + loginResponse.token)
-            .setPriority(Priority.HIGH)
-            .build()
-            .getAsJSONObject(object : JSONObjectRequestListener {
-                override fun onResponse(response: JSONObject) {
-                    productsList = gson.fromJson(response.toString(), ProductsList::class.java)
-                    productsListAdapter.add(productsList)
-                    productsListAdapter.setClickListener(itemClickListener = object :
-                        ItemClickListener {
-                        override fun onItemClick(view: View?, position: Int) {
-                            /*val myBundle = Bundle()
-                            myBundle.putParcelable("ITEM", productsListAdapter.getItem(position))
-                            val myIntent = Intent(
-                                this@ProductsListActivity,
-                                ProductDetailsActivity::class.java
-                            ).putExtra(
-                                "PARCELABLE",
-                                myBundle
-                            )
-                            myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            startActivity(myIntent)*/
-                        }
-                    })
-                }
-
-                override fun onError(error: ANError) {
-                    Log.e("FastError", error.message!!)
-                }
-            })
+//        AndroidNetworking.initialize(applicationContext)
+//        AndroidNetworking.get("https://assessment-sn12.halan.io/products")
+//            .addHeaders("Authorization", "Bearer " + loginResponse.token)
+//            .setPriority(Priority.HIGH)
+//            .build()
+//            .getAsJSONObject(object : JSONObjectRequestListener {
+//                override fun onResponse(response: JSONObject) {
+//                    productsList = gson.fromJson(response.toString(), ProductsList::class.java)
+//                    productsListAdapter.add(productsList)
+//                    productsListAdapter.setClickListener(itemClickListener = object :
+//                        ItemClickListener {
+//                        override fun onItemClick(view: View?, position: Int) {
+//                            /*val myBundle = Bundle()
+//                            myBundle.putParcelable("ITEM", productsListAdapter.getItem(position))
+//                            val myIntent = Intent(
+//                                this@ProductsListActivity,
+//                                ProductDetailsActivity::class.java
+//                            ).putExtra(
+//                                "PARCELABLE",
+//                                myBundle
+//                            )
+//                            myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//                            startActivity(myIntent)*/
+//                        }
+//                    })
+//                }
+//
+//                override fun onError(error: ANError) {
+//                    Log.e("FastError", error.message!!)
+//                }
+//            })
         userName.text = loginResponse.profile.name
         phoneNumber.text = loginResponse.profile.phone
         email.text = loginResponse.profile.email
